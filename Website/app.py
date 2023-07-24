2b9d220bd3fb2ae0ae19bbc6eb2dacc6d844f430
SZSage <simonz@uoregon.edu>
2023-07-23 20:35:14 -0700

    added backend framework with a simple GET request for html

---
+++ Website/app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI 
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import os
+
+app = FastAPI()
+
+
+# mount static files directories (only for static file like css)
+app.mount("/css", StaticFiles(directory="css"), name="css")
+
+
+# this function does nothing yet
+@app.get("/data")
+async def get_data():
+    # replace with actual data
+    return {"message": "Test"}
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    # reads Wordle.html file and returns html file
+    with open(os.path.join('html', 'Wordle.html'), 'r') as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content, status_code=200)
