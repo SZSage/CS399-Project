d96791db6aefe449a45efb4de03706f468bd7f79
JeffTanChina <93405821+JeffTanChina@users.noreply.github.com>
2023-08-04 14:45:28 -0700

    Create link, some little change
    Create links for all the subpages and fix some Lower letter and Upper letter bug

--- Website/app.py
+++ Website/app.py
@@ -1,22 +1,23 @@
-from fastapi import FastAPI 
+import uvicorn
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import os
 
 app = FastAPI()
 
-
-
 # mount static files directories (only for static file like css)
 app.mount("/css", StaticFiles(directory="css"), name="css")
 app.mount("/js", StaticFiles(directory="js"), name="js")
 app.mount("/images", StaticFiles(directory="images"), name="images")
+
 
 # this function does nothing yet
 @app.get("/data")
 async def get_data():
     # replace with actual data
     return {"message": "Test"}
+
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
@@ -25,9 +26,41 @@ async def read_root():
         html_content = file.read()
     return HTMLResponse(content=html_content, status_code=200)
 
-@app.get("/game_description", response_class=HTMLResponse)
+
+@app.get("/Game_description", response_class=HTMLResponse)
 async def read_description():
     with open(os.path.join("html", "Game_description.html"), "r") as file:
         html_content = file.read()
     return HTMLResponse(content=html_content, status_code=200)
 
+
+@app.get("/Login", response_class=HTMLResponse)
+async def read_description():
+    with open(os.path.join("html", "Login.html"), "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.get("/Company", response_class=HTMLResponse)
+async def read_description():
+    with open(os.path.join("html", "Company.html"), "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.get("/Contact_us", response_class=HTMLResponse)
+async def read_description():
+    with open(os.path.join("html", "Contact_us.html"), "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.get("/Login/CreateUser", response_class=HTMLResponse)
+async def read_description():
+    with open(os.path.join("html", "CreateUser.html"), "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
